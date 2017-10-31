36f0e2c55247053d02cf955eb1cfc2338a270dad
JazminAyala <tarin_ayala@hotmail.com>
2017-10-31 10:53:23 -0600

    segunda version

--- tarjeta-credito-valida/js/app.js
+++ tarjeta-credito-valida/js/app.js
@@ -1,1 +1,20 @@
-alert ('hola')
+var numbers = ['4','0','0','8','3','9','5','2','0','1','5','2','6','3'];
+
+var isValidCard = function (array){
+  
+  var newArray = [];
+  var size = array.length;
+  var lastPosition = size -1;
+  
+  for (var i = lastPosition; i>=0; i--){
+    /*en el nuevo array se va agregando, utilizando el meto push, se guardan los elementos al reves*/
+    newArray.push(array[i]);
+  }
+  //La funcion retorna el nuevo array
+  return newArray;
+  
+  /*invocando a la funcion y pasa como parametro el array que sea, mostrando la solucion en consola*/
+}
+
+console.log(isValidCard(numbers));
+console.log(numbers);
